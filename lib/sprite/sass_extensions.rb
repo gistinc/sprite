266485d9b17506a0db823212f0bd954a4145aac3
Jacques Crocker <merbjedi@gmail.com>
2009-11-18 01:35:09 -0800

    Adding SASS Extension method

--- lib/sprite/sass_extensions.rb
+++ lib/sprite/sass_extensions.rb
@@ -1,41 +1,48 @@
 module Sprite::Sass::Extensions
   def sprite_background(group, image)
     sprite = sprite_data(group, image)
-    # TODO
-    ""
+    if sprite
+      "url('#{sprite[:img]}') no-repeat #{sprite[:x]}px #{sprite[:y]}px"
+    else
+      ""
+    end
   end
 
   def sprite_width(group, image)
     sprite = sprite_data(group, image)
-    # TODO
-    ""
+    if sprite
+      "#{sprite[:width]}px"
+    else
+      ""
+    end
   end
   
   def sprite_height(group, image)
     sprite = sprite_data(group, image)
-    # TODO
-    ""
+    if sprite
+      "#{sprite[:height]}px"
+    else
+      ""
+    end
   end
   
   protected
   def sprite_data(group, image)
     unless @__sprite_data
-      @__sprite_data = {
-        "group_name" => {
-          "image_name" => {
-            :width => "",
-            :height => "",
-            :group => "dkajsdfk",
-            :x => "",
-            :y => ""
-          }
-        }
-      }
+      
+      # TODO: read template from !sprite_data
+      sprite_data_path = "public/sass/sprites.yml"
+      
+      # figure out the site root
+      root = "./"
+      
+      # read sprite data from yml
+      @__sprite_data = File.open(File.join(root, sprite_data_path)) { |yf| YAML::load( yf ) }
     end
     
-    group_data = @__sprite_data[group]
+    group_data = @__sprite_data[group.to_s]
     if group_data
-      return group_data[image]
+      return group_data[image.to_s]
     else
       nil
     end
